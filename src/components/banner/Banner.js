84a3ad7e8ecb1053c8c8d1f35a2912f89e35f99c
ammmy016 <ammysingh1999@gmail.com>
2021-05-05 23:34:03 +0530

    ..api not working

--- src/components/banner/Banner.js
+++ src/components/banner/Banner.js
@@ -1,9 +1,27 @@
-import React from 'react'
+import React , {useState , useEffect }from 'react'
 import './Banner.css'
-import axios from '../../http-requests/axios'
-import request from '../../http-requests/request'
+// import axios from '../../http-requests/axios'
+import axios from 'axios'
+import requests from '../../http-requests/request'
 
 function Banner() {
+
+    const [movie , setMovie] =  useState([]);
+
+    useEffect(() => {
+        async function fetchData () {
+            const request = await axios.get(`https://www.themoviedb.org/3${requests.fetchNetflixOriginals}`);
+            setMovie(
+                request.data.results[
+                    Math.floor(Math.random()* request.data.results.length - 1)
+                ]
+            );
+            return request
+        }
+        fetchData();
+    }, [])
+
+    console.log(movie);
 
     const bannerStyle = {
         backgroundImage:'url("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQo_Y1UZGjwSwlCkHnoiwJIwQKNw5cVkLDqPQ&usqp=CAU")',
@@ -11,8 +29,8 @@ function Banner() {
         backgroundSize : "cover"
     }
 
-    const minifyText = () => {
-        
+    const minifyText = ( str , n ) => {
+        return str ?.length > n ? str.substr(n-1) + '...' : str
     }
 
     return (
--- src/http-requests/request.js
+++ src/http-requests/request.js
@@ -1,8 +1,9 @@
 const API_KEY = "cae84d3e337dc70d5eef8149b8e343e6"
 
 const request = {
-    ​fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+    // ​fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
     fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+    fetchActionMovies : '/discover/movie?api_key=${API_KEY}&with_genres=28',
     fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`, 
     fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
     fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`, 

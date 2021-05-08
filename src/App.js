4f4b259c36c72e99febe87b7f114fa44655e5e36
ammmy016 <ammysingh1999@gmail.com>
2021-05-08 23:17:18 +0530

    user auth

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-import React from 'react';
+import React , { useEffect } from 'react';
 import './App.css';
 import HomeScreen from './screens/HomeScreen/HomeScreen'
 import Login from "./screens/login/Login"
+import { auth }  from './firebase'
 import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
 
 function App() {
   const user = false;
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged(userAuth => {
+      if(userAuth){
+        //logged in
+        console.log(userAuth);
+      }else{
+
+      }
+    })
+    return unsubscribe
+    
+  }, [])
+
   return (
     <div className="App">
      <Router>
